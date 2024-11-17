66fd6a907ee05859a39e188e5539ecf2889d1779
Victor Gherardi <gherardivictor@gmail.com>
2024-11-17 21:19:35 +0100

    chapter 16

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -8,7 +8,17 @@ import { lusitana } from '@/app/ui/fonts';
 // 	fetchCardData,
 // } from '@/app/lib/data';
 import { Suspense } from 'react';
-import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
+import {
+	RevenueChartSkeleton,
+	LatestInvoicesSkeleton,
+	CardsSkeleton,
+} from '@/app/ui/skeletons';
+
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Dashboard',
+};
 
 export default function Page() {
 	// await new Promise((resolve) => setTimeout(resolve, 4000));
@@ -36,20 +46,20 @@ export default function Page() {
           value={numberOfCustomers}
           type="customers"
         /> */}
-				{/* <Suspense fallback={<CardsSkeleton />}> */}
+				<Suspense fallback={<CardsSkeleton />}>
 					{/* questo singolo fa bloccare tutta la pagina */}
-          <CardWrapper />
-        {/* </Suspense> */}
+					<CardWrapper />
+				</Suspense>
 			</div>
 			<div className='mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8'>
 				{/* <RevenueChart revenue={revenue} /> */}
-				{/* <Suspense fallback={<RevenueChartSkeleton />}> */}
-          <RevenueChart />
-        {/* </Suspense> */}
+				<Suspense fallback={<RevenueChartSkeleton />}>
+					<RevenueChart />
+				</Suspense>
 				{/* <LatestInvoices latestInvoices={LatestInvoicesSkeleton} /> */}
-				{/* <Suspense fallback={<LatestInvoicesSkeleton />}> */}
-          <LatestInvoices />
-        {/* </Suspense> */}
+				<Suspense fallback={<LatestInvoicesSkeleton />}>
+					<LatestInvoices />
+				</Suspense>
 			</div>
 		</main>
 	);
