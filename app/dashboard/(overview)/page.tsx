2d62be5609e225c9407efa99af76f77e6ee33832
Victor Gherardi <gherardivictor@gmail.com>
2024-11-18 15:32:17 +0100

    adding pagination

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,11 +2,6 @@ import CardWrapper from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-// import {
-// 	// fetchRevenue,
-// 	// fetchLatestInvoices,
-// 	fetchCardData,
-// } from '@/app/lib/data';
 import { Suspense } from 'react';
 import {
 	RevenueChartSkeleton,
@@ -21,42 +16,20 @@ export const metadata: Metadata = {
 };
 
 export default function Page() {
-	// await new Promise((resolve) => setTimeout(resolve, 4000));
-	// const revenue = await fetchRevenue();
-	// const latestInvoices = await fetchLatestInvoices();
-
-	// const {
-	// 	numberOfCustomers,
-	// 	numberOfInvoices,
-	// 	totalPaidInvoices,
-	// 	totalPendingInvoices,
-	// } = await fetchCardData();
-
 	return (
 		<main>
 			<h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
 				Dashboard
 			</h1>
 			<div className='grid gap-6 sm:grid-cols-2 lg:grid-cols-4'>
-				{/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
-				<Card title="Pending" value={totalPendingInvoices} type="pending" />
-				<Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-				<Card
-          title="Total Customers"
-          value={numberOfCustomers}
-          type="customers"
-        /> */}
 				<Suspense fallback={<CardsSkeleton />}>
-					{/* questo singolo fa bloccare tutta la pagina */}
 					<CardWrapper />
 				</Suspense>
 			</div>
 			<div className='mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8'>
-				{/* <RevenueChart revenue={revenue} /> */}
 				<Suspense fallback={<RevenueChartSkeleton />}>
 					<RevenueChart />
 				</Suspense>
-				{/* <LatestInvoices latestInvoices={LatestInvoicesSkeleton} /> */}
 				<Suspense fallback={<LatestInvoicesSkeleton />}>
 					<LatestInvoices />
 				</Suspense>
